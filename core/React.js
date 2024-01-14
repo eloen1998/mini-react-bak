e30fe8f1b05433f5844ae061bfeb3af5fd817060
sunlilun <sunlilun@kanzhun.com>
2024-01-14 20:44:34 +0800

    重构代码

--- core/React.js
+++ core/React.js
@@ -14,11 +14,82 @@ function createElement(type, props, ...children) {
 function createTextNode(text) {
   return {
     type: "TEXT-ELEMENT",
-    nodeValue: text
+    props: {
+      nodeValue: text
+    }
   };
 }
 
+function createDom(type) {
+  return type === "TEXT-ELEMENT" ? document.createTextNode("") : document.createElement(type);
+}
+
+function updateProps(dom, props) {
+  // Object.keys(props).forEach((key) => {
+  //   dom[key] = props[key];
+  // });
+  Object.assign(dom, props);
+}
+
+function runTask(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber.type);
+
+    updateProps(fiber.dom, fiber.props);
+
+    fiber.parent.dom.append(fiber.dom);
+  }
+
+  if (fiber.children) {
+    let prevChild = null;
+    fiber.children.forEach((child) => {
+      const newFiber = {
+        type: child.type,
+        props: child.props,
+        children: child.children,
+        child: null,
+        parent: fiber,
+        sibling: null,
+        dom: null
+      };
+      if (prevChild) {
+        prevChild.sibling = newFiber;
+      } else {
+        fiber.child = newFiber;
+        prevChild = child;
+      }
+    });
+  }
+
+  if (fiber.child) {
+    return fiber.child;
+  }
+
+  if (fiber.sibling) {
+    return fiber.sibling;
+  }
+
+  return fiber.parent?.sibling;
+}
+
+function render(el, container) {
+  let nextTask = {
+    dom: container,
+    children: [el]
+  };
+  function runTaskQueue(deadline) {
+    while (deadline.timeRemaining() > 1 && nextTask) {
+      nextTask = runTask(nextTask);
+    }
+
+    requestIdleCallback(runTaskQueue);
+  }
+
+  requestIdleCallback(runTaskQueue);
+}
+
 export default {
+  render,
   createTextNode,
   createElement
 };
--- core/ReactDom.js
+++ core/ReactDom.js
@@ -1,73 +1,10 @@
-function render(el, parent) {
-  let dom;
-  if (el.type === "TEXT-ELEMENT") {
-    dom = document.createTextNode(el.nodeValue);
-  } else {
-    dom = document.createElement(el.type);
-
-    Object.assign(dom, el.props);
-    if (el.children) {
-      el.children.forEach((child) => {
-        render(child, dom);
-      });
-    }
-  }
-
-  parent.append(dom);
-}
-
-function runTask(task) {
-  const { el, parent, sibling } = task;
-
-  let nextTask = null;
-
-  let dom;
-  if (el.type === "TEXT-ELEMENT") {
-    dom = document.createTextNode(el.nodeValue);
-  } else {
-    dom = document.createElement(el.type);
-
-    Object.assign(dom, el.props);
-  }
-
-  parent.append(dom);
-
-  if (el.children) {
-    nextTask = {
-      parent: dom,
-      sibling: el.children[1],
-      el: el.children[0]
-    };
-    return nextTask;
-  }
-
-  if (sibling) {
-    return sibling;
-  }
-
-  if (parent) {
-    return parent?.sibling;
-  }
-}
-
-
-let nextTask = null;
-function runTaskQueue(deadline) {
-  while (deadline.timeRemaining() > 1 && nextTask) {
-    nextTask = runTask(nextTask);
-  }
-
-  requestIdleCallback(runTaskQueue);
-}
-
-requestIdleCallback(runTaskQueue);
+import React from "./React.js";
 
 const ReactDOM = {
   createRoot(rootDom) {
     return {
       render(el) {
-        nextTask = { el, parent: rootDom };
-        requestIdleCallback(runTaskQueue);
+        React.render(el, rootDom);
       }
     };
   }
