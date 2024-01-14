864c2fe656ea881177d87c652538998e63f617c5
sunlilun <sunlilun@kanzhun.com>
2024-01-14 20:01:16 +0800

    简单实现调度器

--- core/ReactDom.js
+++ core/ReactDom.js
@@ -16,11 +16,58 @@ function render(el, parent) {
   parent.append(dom);
 }
 
+function runTask(task) {
+  const { el, parent, sibling } = task;
+
+  let nextTask = null;
+
+  let dom;
+  if (el.type === "TEXT-ELEMENT") {
+    dom = document.createTextNode(el.nodeValue);
+  } else {
+    dom = document.createElement(el.type);
+
+    Object.assign(dom, el.props);
+  }
+
+  parent.append(dom);
+
+  if (el.children) {
+    nextTask = {
+      parent: dom,
+      sibling: el.children[1],
+      el: el.children[0]
+    };
+    return nextTask;
+  }
+
+  if (sibling) {
+    return sibling;
+  }
+
+  if (parent) {
+    return parent?.sibling;
+  }
+}
+
+
+let nextTask = null;
+function runTaskQueue(deadline) {
+  while (deadline.timeRemaining() > 1 && nextTask) {
+    nextTask = runTask(nextTask);
+  }
+
+  requestIdleCallback(runTaskQueue);
+}
+
+requestIdleCallback(runTaskQueue);
+
 const ReactDOM = {
   createRoot(rootDom) {
     return {
       render(el) {
-        render(el, rootDom);
+        nextTask = { el, parent: rootDom };
+        requestIdleCallback(runTaskQueue);
       }
     };
   }
